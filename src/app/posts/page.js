6bfc4677dff12d5d33ce2ca313b2319ff2dc7ea1
Michal Winiarski <41969647+viniarski@users.noreply.github.com>
2024-03-03 15:24:53 +0000

    fixes

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,4 +1,5 @@
 import { sql } from '@vercel/postgres';
+import Link from 'next/link';
 
 export default async function Page({ params }) {
   const result = await sql`SELECT * FROM guestbook2`;
@@ -8,16 +9,16 @@ export default async function Page({ params }) {
   return (
     <div className="flex flex-col items-center justify-center">
       <div>
-        <a href="/post" className="hover-bold">
+        <a href="/post" className="hover-bold bg-[#00ADB5] rounded-md p-2">
           Add New Post
         </a>
       </div>
-      <div className="mt-4">
-        <h2 className="text-xl font-bold mb-2">Recent Posts:</h2>
+      <div className="mt-4 w-full max-w-3xl">
+        <h2 className="text-xl font-bold mb-2 pt-10">Recent Posts:</h2>
         {Array.isArray(posts) &&
           posts.map((post) => (
             <div key={post.id} className="border rounded p-2 mb-2">
-              <p className="font-bold">{post.username}</p>
+              <p className="font-bold text-[#00ADB5]">{post.username}</p>
               <p>{post.post}</p>
             </div>
           ))}
