add6b07f77cf2ab913e3f21a0630aaaf743059a6
Michal Winiarski <41969647+viniarski@users.noreply.github.com>
2024-03-08 01:27:02 +0000

    favicon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,13 @@ export default function Footer() {
   return (
     <footer>
       <div className="flex justify-center py-2">
-        <p className="text-[#387ADF]">Created by Viniarski 2024</p>
+        <p className="text-[#387ADF]">
+          Created by{' '}
+          <a className="text-[#50C4ED]" href="http://viniarski.com">
+            Viniarski
+          </a>{' '}
+          2024
+        </p>
       </div>
     </footer>
   );
