5997df5e986b6f1f3dcdc1219f54ec5752526ef1
Michal Winiarski <41969647+viniarski@users.noreply.github.com>
2024-03-10 00:47:44 +0000

    add profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 // src/components/Header.jsx
 
 import Link from 'next/link';
-import { SignInButton } from '@clerk/nextjs';
+import Image from 'next/image';
 import { UserButton } from '@clerk/nextjs';
 import { auth } from '@clerk/nextjs';
+import homeImage from './home.png';
+import postsImage from './posts.png';
+import profileImage from './profile.png';
 
 export default function Header() {
   const { userId } = auth();
+
   return (
     <div className="flex justify-center">
       <div className="p-4 pt-14">
@@ -15,24 +19,55 @@ export default function Header() {
         </div>
         <ol className="flex flex-row p-3 items-center justify-center">
           <li className="m-4">
-            <Link
-              href="/"
-              className="hover:underline"
-              style={{ color: '#387ADF' }}
-            >
-              Home
+            <Link href="/" className="flex items-center hover:scale-110">
+              <Image
+                src={homeImage}
+                alt="Home"
+                width={22}
+                height={22}
+                className="mr-1"
+              />
+              <span style={{ color: '#387ADF' }}>Home</span>
             </Link>
           </li>
           <li className="m-4">
-            <Link
-              href="/posts"
-              className="hover:underline"
-              style={{ color: '#387ADF' }}
-            >
-              Posts
+            <Link href="/posts" className="flex items-center hover:scale-110">
+              <Image
+                src={postsImage}
+                alt="Posts"
+                width={16}
+                height={16}
+                className="mr-1"
+              />
+              <span style={{ color: '#387ADF' }}>Posts</span>
             </Link>
           </li>
-          <li className="m-4">{userId ? <UserButton /> : <SignInButton />}</li>
+          {userId && (
+            <li className="m-4">
+              <Link
+                href={`/user/${userId}`}
+                className="flex items-center hover:scale-110"
+              >
+                <Image
+                  src={profileImage}
+                  alt="Profile"
+                  width={14}
+                  height={14}
+                  className="mr-1"
+                />
+                <span style={{ color: '#387ADF' }}>Profile</span>
+              </Link>
+            </li>
+          )}
+          <li className="m-4">
+            {userId ? (
+              <UserButton />
+            ) : (
+              <Link href="/sign-in" className="text-white hover:scale-110">
+                Sign In
+              </Link>
+            )}
+          </li>
         </ol>
       </div>
     </div>
